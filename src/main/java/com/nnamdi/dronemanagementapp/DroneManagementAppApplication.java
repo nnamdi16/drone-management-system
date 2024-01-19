cb578bccc05dcd6d0500d606d2d66c66a35642b2
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-01-19 22:39:42 +0100

    initial commit

---
+++ src/main/java/com/nnamdi/dronemanagementapp/DroneManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.nnamdi.dronemanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nnamdi/dronemanagementapp/DroneManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnamdi.dronemanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

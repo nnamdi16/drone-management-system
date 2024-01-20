23a9e05e22447f6de9922fd6cec611eafd88cb0a
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-01-20 23:08:26 +0100

    chore: Implemented ConstantsUtil to handle constants

---
+++ src/main/java/com/nnamdi/dronemanagementapp/util/ConstantsUtil.java
@@ -0,0 +1,11 @@
+package com.nnamdi.dronemanagementapp.util;
+
+public class ConstantsUtil {
+    private ConstantsUtil() {
+    }
+
+    public static final String BAD_REQUEST = "Bad Request";
+    public static final String NOT_FOUND = "Entity not found";
+
+    public static final String SUCCESSFUL = "Request processed successfully";
+}

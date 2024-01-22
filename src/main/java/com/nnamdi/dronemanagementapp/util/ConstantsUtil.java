1533f877fbbe1a5aa0bb16b42bf57ac0fb2474f0
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-01-22 22:55:30 +0100

    chore: Implemented ConstantsUtil for response message

--- src/main/java/com/nnamdi/dronemanagementapp/util/ConstantsUtil.java
+++ src/main/java/com/nnamdi/dronemanagementapp/util/ConstantsUtil.java
@@ -6,6 +6,7 @@ public class ConstantsUtil {
 
     public static final String BAD_REQUEST = "Bad Request";
     public static final String NOT_FOUND = "Entity not found";
+    public static final String ALREADY_EXIST = "Entity already exist";
 
     public static final String SUCCESSFUL = "Request processed successfully";
 }

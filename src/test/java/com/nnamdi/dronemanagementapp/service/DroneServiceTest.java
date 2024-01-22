8f424f2977c0a6ce203bfbdcef431e04723c177c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-01-22 23:12:43 +0100

    chore: Implemented test for registerDrone method

--- src/test/java/com/nnamdi/dronemanagementapp/service/DroneServiceTest.java
+++ src/test/java/com/nnamdi/dronemanagementapp/service/DroneServiceTest.java
@@ -1,17 +1,26 @@
 package com.nnamdi.dronemanagementapp.service;
 
-import com.nnamdi.dronemanagementapp.dto.Response;
-import com.nnamdi.dronemanagementapp.request.DroneDto;
+import com.nnamdi.dronemanagementapp.exception.ModelAlreadyExistException;
+import com.nnamdi.dronemanagementapp.model.Drone;
+import com.nnamdi.dronemanagementapp.repository.DroneRepository;
+import com.nnamdi.dronemanagementapp.request.RegisterDroneDto;
 import com.nnamdi.dronemanagementapp.service.impl.DroneServiceImpl;
+import com.nnamdi.dronemanagementapp.util.Direction;
+import com.nnamdi.dronemanagementapp.util.DroneUtil;
 import com.nnamdi.dronemanagementapp.util.ResponseUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import reactor.test.StepVerifier;
+import org.modelmapper.ModelMapper;
 
-import static com.nnamdi.dronemanagementapp.mock.TestMock.buildResponse;
+import java.util.Optional;
+
+import static com.nnamdi.dronemanagementapp.mock.TestMock.buildDrone;
 import static com.nnamdi.dronemanagementapp.mock.TestMock.registerDroneDto;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 
@@ -20,24 +29,55 @@ class DroneServiceTest {
     @Mock
     ResponseUtil responseUtil;
 
-
     @Mock
     private  DroneService droneService;
+
+    @Mock
+    private DroneRepository droneRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @Mock
+    private DroneUtil droneUtil;
 
 
     @BeforeEach
     public void setup(){
         MockitoAnnotations.openMocks(this);
-        droneService = new DroneServiceImpl(responseUtil);
+        modelMapper = new ModelMapper();
+        droneService = new DroneServiceImpl(modelMapper, droneUtil, droneRepository);
     }
 
     @Test
     void registerDrone() {
-        DroneDto drone = registerDroneDto();
+        RegisterDroneDto droneDto = registerDroneDto();
+        Drone drone = buildDrone(droneDto);
+        when(droneRepository.save(any(Drone.class))).thenReturn(drone);
+        when(droneRepository.findByCoordinates(droneDto.getCoordinateX(), droneDto.getCoordinateY())).thenReturn(Optional.empty());
+        when(droneUtil.convertDtoToEntity(droneDto)).thenReturn(drone);
+        final var response  = droneService.registerDrone(droneDto);
+        assertThat(response).isNotNull();
+        assertThat(response.getCoordinateX()).isNotNegative();
+        assertThat(response.getCoordinateY()).isNotNegative();
+        assertThat(response.getCoordinateX()).isLessThanOrEqualTo(10);
+        assertThat(response.getCoordinateY()).isLessThanOrEqualTo(10);
+        assertThat(response.getDirection()).isIn(Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH);
 
-        Response response = buildResponse(drone);
-        when(responseUtil.getSuccessResponse(drone)).thenReturn(response);
-        StepVerifier.create(droneService.registerDrone(drone)).expectNext(response).verifyComplete();
+
+    }
+
+    @Test
+    void registerDroneAlreadyExist() {
+        RegisterDroneDto droneDto = registerDroneDto();
+        Drone drone = buildDrone(droneDto);
+        when(droneUtil.convertDtoToEntity(droneDto)).thenReturn(drone);
+
+        when(droneRepository.findByCoordinates(droneDto.getCoordinateX(), droneDto.getCoordinateY())).thenReturn(Optional.of(drone));
+
+        assertThatThrownBy(() -> droneService.registerDrone(droneDto)).hasMessage("Entity already exist").isInstanceOf(ModelAlreadyExistException.class);
+
+
     }
 
 

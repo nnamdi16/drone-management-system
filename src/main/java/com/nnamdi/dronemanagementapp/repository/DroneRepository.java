0644f9627463a26382dfe9df9350ee8535be16fd
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-01-22 23:09:00 +0100

    chore: Implemented findByCoordinates method to find a drone that is within a coordinate

---
+++ src/main/java/com/nnamdi/dronemanagementapp/repository/DroneRepository.java
@@ -0,0 +1,16 @@
+package com.nnamdi.dronemanagementapp.repository;
+
+import com.nnamdi.dronemanagementapp.model.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DroneRepository extends JpaRepository<Drone, String> {
+
+    @Query("SELECT d FROM Drone d WHERE d.coordinateX = :coordinateX AND d.coordinateY = :coordinateY")
+    Optional<Drone> findByCoordinates(@Param("coordinateX") int coordinateX, @Param("coordinateY")int coordinateY);
+}

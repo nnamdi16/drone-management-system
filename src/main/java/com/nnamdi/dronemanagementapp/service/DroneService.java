379f78cfc905ee902a6d99eaa1826904fa572b2f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-01-23 11:55:30 +0100

    chore: Refactored the moveDrone method

--- src/main/java/com/nnamdi/dronemanagementapp/service/DroneService.java
+++ src/main/java/com/nnamdi/dronemanagementapp/service/DroneService.java
@@ -3,7 +3,7 @@ package com.nnamdi.dronemanagementapp.service;
 import com.nnamdi.dronemanagementapp.dto.DroneDto;
 import com.nnamdi.dronemanagementapp.model.Drone;
 import com.nnamdi.dronemanagementapp.request.RegisterDroneDto;
-import com.nnamdi.dronemanagementapp.request.UpdateDroneDto;
+import com.nnamdi.dronemanagementapp.request.UpdateDronePositionDto;
 
 import java.util.Optional;
 
@@ -13,6 +13,6 @@ import java.util.Optional;
  */
 public interface DroneService {
     DroneDto registerDrone(RegisterDroneDto drone);
-    DroneDto moveDrone(String id, UpdateDroneDto updateDroneDto);
+    DroneDto moveDrone(String id, UpdateDronePositionDto updateDroneDto);
     Optional<Drone> getDronePosition(String id);
 }

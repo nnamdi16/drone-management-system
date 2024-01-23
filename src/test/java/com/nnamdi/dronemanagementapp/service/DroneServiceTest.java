5b04ae45a868bd020322091e74a1d29629c33a19
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-01-23 11:54:34 +0100

    chore: Implemented test for moving the drone

--- src/test/java/com/nnamdi/dronemanagementapp/service/DroneServiceTest.java
+++ src/test/java/com/nnamdi/dronemanagementapp/service/DroneServiceTest.java
@@ -1,9 +1,12 @@
 package com.nnamdi.dronemanagementapp.service;
 
+import com.nnamdi.dronemanagementapp.exception.BadRequestException;
 import com.nnamdi.dronemanagementapp.exception.ModelAlreadyExistException;
+import com.nnamdi.dronemanagementapp.exception.NotFoundException;
 import com.nnamdi.dronemanagementapp.model.Drone;
 import com.nnamdi.dronemanagementapp.repository.DroneRepository;
 import com.nnamdi.dronemanagementapp.request.RegisterDroneDto;
+import com.nnamdi.dronemanagementapp.request.UpdateDronePositionDto;
 import com.nnamdi.dronemanagementapp.service.impl.DroneServiceImpl;
 import com.nnamdi.dronemanagementapp.util.Direction;
 import com.nnamdi.dronemanagementapp.util.DroneUtil;
@@ -55,7 +58,7 @@ class DroneServiceTest {
         Drone drone = buildDrone(droneDto);
         when(droneRepository.save(any(Drone.class))).thenReturn(drone);
         when(droneRepository.findByCoordinatesOrName(droneDto.getCoordinateX(), droneDto.getCoordinateY(), droneDto.getName())).thenReturn(Optional.empty());
-        when(droneUtil.convertDtoToEntity(droneDto)).thenReturn(drone);
+        when(droneUtil.buildDroneEntity(droneDto)).thenReturn(drone);
         final var response  = droneService.registerDrone(droneDto);
         assertThat(response).isNotNull();
         assertThat(response.getCoordinateX()).isNotNegative();
@@ -69,7 +72,7 @@ class DroneServiceTest {
     void registerDroneAlreadyExist() {
         RegisterDroneDto droneDto = registerDroneDto();
         Drone drone = buildDrone(droneDto);
-        when(droneUtil.convertDtoToEntity(droneDto)).thenReturn(drone);
+        when(droneUtil.buildDroneEntity(droneDto)).thenReturn(drone);
         when(droneRepository.findByCoordinatesOrName(droneDto.getCoordinateX(), droneDto.getCoordinateY(), droneDto.getName())).thenReturn(Optional.of(drone));
         assertThatThrownBy(() -> droneService.registerDrone(droneDto)).hasMessage("Entity already exist").isInstanceOf(ModelAlreadyExistException.class);
     }
@@ -88,6 +91,47 @@ class DroneServiceTest {
         when(droneRepository.findById(anyString())).thenReturn(Optional.empty());
         final var response  = droneService.getDronePosition(anyString());
         assertThat(response).isEmpty();
+    }
+
+    @Test
+    void moveDrone() {
+        RegisterDroneDto registerDroneDto = registerDroneDto();
+        Drone drone = buildDrone(registerDroneDto);
+        UpdateDronePositionDto updateDronePositionDto = updateDronePositionDto();
+        Drone updateDrone = updateDrone(updateDronePositionDto);
+        when(droneRepository.save(any(Drone.class))).thenReturn(updateDrone);
+        when(droneRepository.findById(anyString())).thenReturn(Optional.of(drone));
+        when(droneUtil.isValidDirectionChange(drone.getDirection(), updateDronePositionDto.getDirection())).thenReturn(true);
+        final var response  = droneService.moveDrone(ID,updateDronePositionDto);
+        assertThat(response).isNotNull();
+        assertThat(response.getCoordinateX()).isNotNegative();
+        assertThat(response.getCoordinateY()).isNotNegative();
+        assertThat(response.getCoordinateX()).isLessThanOrEqualTo(10);
+        assertThat(response.getCoordinateY()).isLessThanOrEqualTo(10);
+        assertThat(response.getDirection()).isIn(Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH);
+    }
+
+
+    @Test
+    void moveDroneNotFoundException() {
+
+        UpdateDronePositionDto updateDronePositionDto = updateDronePositionDto();
+        when(droneRepository.findById(anyString())).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> droneService.moveDrone(ID, updateDronePositionDto)).hasMessage("Entity not found").isInstanceOf(NotFoundException.class);
+    }
+
+
+    @Test
+    void moveDroneBadRequest() {
+        RegisterDroneDto registerDroneDto = registerDroneDto();
+        Drone drone = buildDrone(registerDroneDto);
+        UpdateDronePositionDto updateDronePositionDto = updateDronePositionDto();
+        Drone updateDrone = updateDrone(updateDronePositionDto);
+        when(droneRepository.save(any(Drone.class))).thenReturn(updateDrone);
+        when(droneRepository.findById(anyString())).thenReturn(Optional.of(drone));
+        when(droneUtil.isValidDirectionChange(drone.getDirection(), updateDronePositionDto.getDirection())).thenReturn(false);
+
+        assertThatThrownBy(() -> droneService.moveDrone(ID, updateDronePositionDto)).hasMessage("Invalid direction from " + drone.getDirection() + " to " + updateDronePositionDto.getDirection()).isInstanceOf(BadRequestException.class);
     }
 
 

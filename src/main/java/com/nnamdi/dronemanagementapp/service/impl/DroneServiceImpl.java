500200ec5e7ad84680f9cb6e66d3fbd5eac2c008
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-06-02 18:49:17 +0200

    chore: commented unused lines of code

--- src/main/java/com/nnamdi/dronemanagementapp/service/impl/DroneServiceImpl.java
+++ src/main/java/com/nnamdi/dronemanagementapp/service/impl/DroneServiceImpl.java
@@ -84,7 +84,7 @@ public class DroneServiceImpl implements DroneService {
     public PageImpl<DroneDto> getDrones(int page, int limit) {
         log.info("about to retrieve all notes by pagination {}, {}", page, limit);
         AppUtil.validatePageRequest(page, limit);
-        Pageable pageable = PageRequest.of(page - 1, limit);
+//        Pageable pageable = PageRequest.of(page - 1, limit);
 //        Page<Drone> drones = droneRepository.findAll(pageable);
        return apiService.getDrones(page, limit);
 //        List<DroneDto> notesDtoList = drones.getContent().stream().map(note -> modelMapper.map(note, DroneDto.class)).toList();

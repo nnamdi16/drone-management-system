261b41239581b4a383ae38a40c432e96220e2812
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-08-01 12:30:11 +0200

    chore: added DotenvInitializer

--- src/main/java/com/nnamdi/dronemanagementapp/DroneManagementAppApplication.java
+++ src/main/java/com/nnamdi/dronemanagementapp/DroneManagementAppApplication.java
@@ -1,5 +1,6 @@
 package com.nnamdi.dronemanagementapp;
 
+import com.nnamdi.dronemanagementapp.config.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DroneManagementAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DroneManagementAppApplication.class, args);
+//        SpringApplication.run(DroneManagementAppApplication.class, args);
+        SpringApplication app = new SpringApplication(DroneManagementAppApplication.class);
+        app.addInitializers(new DotenvInitializer());
+        app.run(args);
     }
 
 }
